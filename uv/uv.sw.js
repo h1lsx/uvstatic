817d83ebdffac80ef1b9aedb2ef75640cc5ef1b5
h1lsx <139397050+h1lsx@users.noreply.github.com>
2023-10-12 16:20:23 -0400

    Update uv.sw.js

--- uv/uv.sw.js
+++ uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/playground/uv/uv.bundle.js');
-importScripts('/playground/uv/uv.config.js');
+importScripts('/uvstatic/uv/uv.bundle.js');
+importScripts('/uvstatic/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
